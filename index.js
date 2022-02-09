51e6c1ad0bb8e5a17ac6ef09c3961495c4a69280
Swapnil <swapniljain240@gmail.com>
2022-02-10 00:41:50 +0530

    setting express layouts partials

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 8001;
+const expressLayouts = require('express-ejs-layouts');
 
 
+//allowing our app to use static files from assets forlder gobally
+app.use(express.static('./assets'));
 
 
-
+//setting up embeddedjavascript
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-//using express router to manage diff client requests
+//using layouts and partials 
+app.use(expressLayouts);
+app.set("layout extractScripts", true)
+app.set("layout extractStyles", true)
 
+//using express router to manage diff client requests
 app.use('/',require('./routes'));
 
 
